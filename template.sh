6f59e918ed7050eb08a71f8e7fa2aff723dabf5a
sdabh <sdabh@uic.edu>
2026-03-14 18:30:16 -0500

    updated folder structure

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/medguide-notebook.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory & Files created successfully!"
